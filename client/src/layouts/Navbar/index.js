dea96f4537074e726eb9f299dca76698abafe9bc
Fourth1755 <fourthe.hero@gmail.com>
2022-11-04 13:21:03 +0700

    create navbar

---
+++ client/src/layouts/Navbar/index.js
@@ -0,0 +1,34 @@
+import './index.scss'
+//import SearchBarAnime from '../SearchBarAnime';
+//import ProfileBar from '../ProfileBar';
+import { Link } from "react-router-dom";
+//import { getUser } from "../../servies/authorize";
+import { useState } from 'react';
+
+const Navbar=()=>{
+    //const user=getUser()
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+    return(
+        <div className='navigation'>
+            <div className='navigation-left'>
+                <div className='navigation-logo'>
+                    <h1><Link to='/'>AniMap</Link></h1>
+                </div>
+                <div className="navigation-menu">
+                    <ul>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to="/allanime">Anime</Link></li>
+                        <li><Link to="/allstudio">Studio</Link></li>
+                        <li><Link to="/topanime">Top Anime</Link></li>
+                        {/* {user?<li><Link to="/mymap">My Map</Link></li>:<></>} */}
+                    </ul>
+                </div>
+            </div>
+            <div className='navigation-right'>
+                {/* <SearchBarAnime/> */}
+                {/* {user?<ProfileBar/>:<Link to="/login" className='navbar-login-button' >Login</Link>} */}
+            </div>
+        </div>
+    )
+}
+export default Navbar;
