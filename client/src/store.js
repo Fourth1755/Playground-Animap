cbfd6f062c17f32af90cf3ae3f0a3637f27bf29a
Fourth1755 <fourthe.hero@gmail.com>
2022-11-14 22:13:26 +0700

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -2,14 +2,17 @@ import { configureStore } from '@reduxjs/toolkit'
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { animeApi } from './services/anime'
+import { tagApi } from './services/tag'
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
     [animeApi.reducerPath]: animeApi.reducer,
+    [tagApi.reducerPath]: tagApi.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(animeApi.middleware),
+    getDefaultMiddleware().concat(animeApi.middleware,tagApi.middleware),
+    
 })
