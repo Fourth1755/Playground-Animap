e37c2330ece6a337b269c1b14a42b4404348e197
Fourth1755 <fourthe.hero@gmail.com>
2022-11-14 22:15:44 +0700

    update AdminTagPage

--- client/src/pages/Admin/AdminTagPage/index.js
+++ client/src/pages/Admin/AdminTagPage/index.js
@@ -2,9 +2,7 @@ import { useState, useEffect } from 'react';
 import "./index.scss";
 import AdminTagTable from "../../../components/table/AdminTagTable";
 import AdminTagModal from "../../../components/modal/AdminTagModal";
-import AdminTableTagAnime from "../../component/AdminTableTagAnime";
-
-
+import { useGetAllTagQuery }from '../../../services/tag'
 const AdminTagPage = () => {
     const [modalTag, setModalTag] = useState()
     const [modalModeTag, setModalModeTag] = useState('')
@@ -17,28 +15,35 @@ const AdminTagPage = () => {
         }
         setOpenTag(true);
     }
-  return (
-    <div>
-        <div className="adminAnime-header">
-            <h1>Manage Tag Anime</h1>
-        </div>
-        <div className="adminAnimeTag-container">
-            <div className="adminAnimeTag-container-table1">
-                <div className="adminAnimeTag-container-header">
-                   <h2>Tag</h2>
-                    <button onClick={() => handleOpenModalTag([], "create")}>Add Tag</button> 
-                </div>
-                <AdminTagTable />
+    const { data, error, isLoading } = useGetAllTagQuery()
+    return (
+        <div>
+            <div className="adminAnime-header">
+                <h1>Manage Tag Anime</h1>
             </div>
-            <div className="adminAnimeTag-container-table2">
-                <div className="adminAnimeTag-container-header">
-                   <h2>Tag Anime</h2>
+            <div className="adminAnimeTag-container">
+                <div className="adminAnimeTag-container-table2">
+                    <div className="adminAnimeTag-container-header">
+                    <h2>Anime</h2>
+                    </div>
+                    {/* <AdminTableTagAnime/> */}
                 </div>
-                {/* <AdminTableTagAnime/> */}
+                <div className="adminAnimeTag-container-table1">
+                    <div className="adminAnimeTag-container-header">
+                    <h2>Tag</h2>
+                        <button onClick={() => handleOpenModalTag([], "create")}>Add Tag</button> 
+                    </div>
+                    {error ? (
+                        <>Oh no, there was an error</>
+                    ) : isLoading ? (
+                        <>Loading...</>
+                    ) : data ? (
+                        <AdminTagTable TagAnime = {data}/>
+                    ) : null}
+                </div>
             </div>
+            {/* <AdminTagModal open={openTag} onClose={handleCloseModalTag} tag={modalTag} mode={modalModeTag}/> */}
         </div>
-        {/* <AdminTagModal open={openTag} onClose={handleCloseModalTag} tag={modalTag} mode={modalModeTag}/> */}
-    </div>
-  );
+    );
 };
 export default AdminTagPage;
